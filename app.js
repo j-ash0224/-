9149dfded2f0784633b53dd7a7a3adf8f2a080f5
PJ <parkjj00.224@gmail.com>
2022-12-27 22:38:46 +0900

    ejs 엔진으로 html문서 분리

--- app.js
+++ app.js
@@ -9,36 +9,15 @@ app.listen(3000, function(){
     console.log("서버 가동");
 });
 
+//ejs를 사용하기 위한 코드
+app.set("views","./views");
+app.set("view engine", "ejs");
+
 //경로 설정
 app.get("/", function(req, res){
-    res.send(`<!DOCTYPE html>
-    <html lang="ko">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        루트 페이지 입니다.
-    </body>
-    </html>`);
+    res.render("home/root"); //ejs 파일 불러오기
 });
 
 app.get("/login", function(req, res){
-    res.send(`<!DOCTYPE html>
-        <html lang="ko">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            로그인 페이지 입니다<br>
-            <input type="text"placeholder="아이디"><br>
-            <input type="text"placeholder="비밀번호"><br>
-            <button>로그인</button>
-        </body>
-        </html>`);
+    res.render("home/login");
 });
