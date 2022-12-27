baecd9f9d5c919b11aead218c328997771a6c0ed
PJ <parkjj00.224@gmail.com>
2022-12-27 22:35:52 +0900

    간단한 로그인 뷰 생성

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
-//http 모듈을 실행
-const http = require("http"); //http 모듈 추출 
-const app = http.createServer(function(req, res){
-    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-    //브라우저가 받은 응답을 한글로 해석해야 한다는 것을 명시
-    //200은 정상 응답, html 문서이며 utf-8코드를 사용하라는 의미
-    if(req.url === "/"){
-        res.end("루트 페이지 입니다.");
-    } else if (req.url === "/login"){
-        res.end("로그인 페이지 입니다.");
-    }
-}); //서버 생성, 경로 지정
+/**********************  메인 코드 ********************/
+
+//express 모듈을 실행
+const express = require("express"); //express 모듈 추출 
+const app = express(); //서버 생성
 
 //서버 실행
-app.listen(3001, function(){
+app.listen(3000, function(){
     console.log("서버 가동");
 });
 
-// /**********************  메인 코드 ********************/
+//경로 설정
+app.get("/", function(req, res){
+    res.send(`<!DOCTYPE html>
+    <html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        루트 페이지 입니다.
+    </body>
+    </html>`);
+});
 
-// //express 모듈을 실행
-// const express = require("express"); //express 모듈 추출 
-// const app = express(); //서버 생성
-
-// //서버 실행
-// app.listen(3000, function(){
-//     console.log("서버 가동");
-// });
-
-// //경로 설정
-// app.get("/", function(req, res){
-//     res.send("루트 페이지 입니다.");
-// });
-
-// app.get("/login", function(req, res){
-//     res.send("로그인 페이지 입니다.");
-// });
+app.get("/login", function(req, res){
+    res.send(`<!DOCTYPE html>
+        <html lang="ko">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            로그인 페이지 입니다<br>
+            <input type="text"placeholder="아이디"><br>
+            <input type="text"placeholder="비밀번호"><br>
+            <button>로그인</button>
+        </body>
+        </html>`);
+});
