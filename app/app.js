8100fd100ef4713459ef74f2e41d667ce3afbfd5
PJ <parkjj00.224@gmail.com>
2022-12-28 16:44:18 +0900

    프런트 js 파일과 연동

--- app/app.js
+++ app/app.js
@@ -8,6 +8,9 @@ const app = express(); //서버 생성
 app.set("views","./app/src/views");
 app.set("view engine", "ejs");
 
+// 프런트 js 파일을 이용하기 위한 미들웨어 등록
+app.use(express.static(`${__dirname}/src/public`))
+
 //라우팅 파일을 사용하기 위한 코드
 const route = require("./src/routes/home/route");
 //라우팅 파일을 읽어와서 변수에 저장
